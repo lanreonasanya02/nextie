655287d2fb6263a4e1e0d268add2eac8c8e2875e
Lanre Adeolu <ladeolu2016@gmail.com>
2024-04-14 02:37:47 +0100

    Completed Events/landing page after login component

--- src/pages/events/index.jsx
+++ src/pages/events/index.jsx
@@ -1,7 +1,10 @@
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
-import Link from "next/link";
-// import { Badge, Card, Page, Spacer, Text } from "@geist-ui/core";
+import { Card, Dot, Grid, Input, Spacer, Text } from "@geist-ui/core";
+import { Calendar, Plus, Search } from "@geist-ui/icons";
+import Image from "next/image";
+import { events } from "@/services/Data";
+import empty from "../../../public/no-event.png";
 
 export default function Events() {
   return (
@@ -15,7 +18,186 @@ export default function Events() {
       />
 
       <div className="events-page">
-        <h2>Welcome back! </h2>
+        <div className="events-header">
+          <div className="welcome">
+            <h2>
+              Welcome back, <strong>Cadbury!</strong>{" "}
+            </h2>
+            <span>Here are the updates you missed while being away</span>
+          </div>
+
+          <div>
+            <button className="create-btn">
+              <span>
+                <Plus />
+              </span>
+              <span>Create an event</span>
+            </button>
+          </div>
+        </div>
+
+        <div className="events-overview">
+          <Grid.Container gap={2} justify="center" height="200px">
+            <Grid xs={6}>
+              <Card
+                width="100%"
+                hoverable
+                style={{
+                  borderTopLeftRadius: "30px",
+                }}
+              >
+                <div className="card-details">
+                  <h2>{events.length === 0 ? 0 : events.length}</h2>
+                  <span>Total events created</span>
+                </div>
+              </Card>
+            </Grid>
+
+            <Grid xs={6}>
+              <Card
+                width="100%"
+                hoverable
+                style={{
+                  background: "#1DB762",
+                  color: "#fff",
+                  borderTopLeftRadius: "30px",
+                }}
+              >
+                <div className="card-details">
+                  <h2>{events.length === 0 ? 0 : 30}</h2>
+                  <span>Tickets sold so far</span>
+                </div>
+              </Card>
+            </Grid>
+
+            <Grid xs={6}>
+              <Card
+                width="100%"
+                hoverable
+                style={{
+                  borderTopLeftRadius: "30px",
+                }}
+              >
+                <div className="card-details">
+                  <h2>{events.length === 0 ? "0%" : "75%"}</h2>
+                  <span>Events Turnover</span>
+                </div>
+              </Card>
+            </Grid>
+
+            <Grid xs={6}>
+              <Card
+                width="100%"
+                hoverable
+                style={{
+                  borderTopLeftRadius: "30px",
+                }}
+              >
+                <div className="card-details">
+                  <h2>{events.length === 0 ? "₦0" : "₦150M"}</h2>
+                  <span>Revenue Generated</span>
+                </div>
+              </Card>
+            </Grid>
+          </Grid.Container>
+        </div>
+
+        <div className="events-body">
+          <h4>My Events</h4>
+
+          <div className="header-tools">
+            <div
+              className="filter"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "16px",
+              }}
+            >
+              <button className="filter-btn active">All</button>
+              <button className="filter-btn">Published</button>
+              <button className="filter-btn">Drafted</button>
+              <button className="filter-btn">Ended</button>
+            </div>
+
+            <div className="search">
+              <Input
+                iconRight={<Search />}
+                placeholder="Enter search here"
+                width={25}
+              />
+            </div>
+          </div>
+
+          <div className="events-cards">
+            {events.length === 0 ? (
+              <Grid.Container gap={2} justify="center">
+                <div
+                  className="no-events"
+                  style={{ textAlign: "center", marginTop: "20px" }}
+                >
+                  <Image src={empty} />
+
+                  <Text h4 mb={0}>
+                    Oops! No events yet.
+                  </Text>
+
+                  <Text type="secondary" small>
+                    Click 'Create Event' to start organizing hassle-free
+                    gatherings!
+                  </Text>
+                </div>
+              </Grid.Container>
+            ) : (
+              <Grid.Container gap={2} justify="center">
+                {events.map((event) => {
+                  return (
+                    <Grid xs>
+                      <Card width="400px" style={{ cursor: "pointer" }}>
+                        <Image
+                          src={event.img}
+                          height="150px"
+                          width="400px"
+                          draggable={false}
+                        />
+                        <Text h4 mb={0}>
+                          {event.title}
+                        </Text>
+
+                        <Text type="secondary" small>
+                          {event.desc}
+                        </Text>
+                        <Spacer />
+
+                        <div className="footer">
+                          <Text
+                            type="#30d667"
+                            style={{ color: "#30d667", fontWeight: "500" }}
+                            span
+                            small
+                          >
+                            {event.price}
+                          </Text>
+
+                          <Dot style={{ margin: "0px 8px" }} />
+
+                          <Text type="secondary" span small>
+                            <span className="card-calendar">
+                              <span>
+                                <Calendar size={13} />
+                              </span>
+                              <span>{event.date}</span>
+                            </span>
+                          </Text>
+                        </div>
+                      </Card>
+                    </Grid>
+                  );
+                })}
+              </Grid.Container>
+            )}
+          </div>
+        </div>
       </div>
 
       <style jsx>{`
@@ -24,9 +206,73 @@ export default function Events() {
           padding: 30px;
         }
 
+        .events-header,
+        .header-tools {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+
         h2 {
-          font-size: 32px;
+          font-size: 30px;
+          font-weight: 500;
+        }
+
+        .create-btn {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 10px;
+          width: 170px;
+          padding: 16px;
+          color: #fff;
+          background-color: #30d667;
+          border-style: none;
+          border: 1px solid #30d667;
+          border-radius: 10px;
           font-weight: 700;
+          cursor: pointer;
+        }
+
+        .filter-btn {
+          padding: 5px 16px;
+          color: #9ca3af;
+          background: none;
+          border-style: none;
+          border: 1px solid #9ca3af;
+          border-radius: 10px;
+          cursor: pointer;
+          font-size: 13px;
+        }
+
+        .active {
+          color: #0e7a3e;
+          background-color: #ecfdf5;
+          border: 1px solid #30d667;
+        }
+
+        .events-overview,
+        .events-body,
+        .events-cards {
+          margin-top: 40px;
+        }
+
+        .card-details {
+          text-align: center;
+          height: 15vh;
+          display: grid;
+          place-content: center;
+        }
+
+        .footer {
+          display: flex;
+          align-items: center;
+        }
+
+        .card-calendar {
+          display: flex;
+          align-items: "center";
+          gap: 10px;
         }
       `}</style>
     </>
---
+++ src/services/Data.js
@@ -0,0 +1,59 @@
+import img1 from "../../public/event-1.png";
+import img2 from "../../public/event-2.png";
+import img3 from "../../public/event-3.png";
+
+export const events = [
+  {
+    id: 1,
+    img: img1,
+    title: "Women in Tech Annual Conference",
+    desc: "Attendees can expect to gain valuable insights, forge meaningful connections...",
+    price: "$30",
+    date: "23 March 2024 5:00 PM GMT+1",
+  },
+
+  {
+    id: 2,
+    img: img2,
+    title: "Demystifying Book Marketing For Self-Published Authors",
+    desc: "Attendees can expect to gain valuable insights, forge meaningful connections...",
+    price: "Free",
+    date: "23 March 2024 5:00 PM GMT+1",
+  },
+
+  {
+    id: 3,
+    img: img3,
+    title: "Happier Life Lessons - with Simon Mundie",
+    desc: "Attendees can expect to gain valuable insights, forge meaningful connections...",
+    price: "Free",
+    date: "23 March 2024 5:00 PM GMT+1",
+  },
+
+  {
+    id: 4,
+    img: img2,
+    title: "Fourth Annual 911der Women Empower Virtual 5K",
+    desc: "Attendees can expect to gain valuable insights, forge meaningful connections...",
+    price: "Free",
+    date: "23 March 2024 5:00 PM GMT+1",
+  },
+
+  {
+    id: 5,
+    img: img3,
+    title: "Out in Tech | Leadership Forum",
+    desc: "Attendees can expect to gain valuable insights, forge meaningful connections...",
+    price: "$30",
+    date: "23 March 2024 5:00 PM GMT+1",
+  },
+
+  {
+    id: 6,
+    img: img1,
+    title: "Using Social Media For Successful Businesses",
+    desc: "Attendees can expect to gain valuable insights, forge meaningful connections...",
+    price: "$30",
+    date: "23 March 2024 5:00 PM GMT+1",
+  },
+];
